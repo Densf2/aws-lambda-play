3c0d8391162a611bef43bc895bac0e1ac22b80d0
Denys Pavlyuk <densf2@gmail.com>
2022-08-15 22:08:13 +0300

    added_handler_for_lambda

--- indes.js
+++ indes.js
@@ -1,2 +1,21 @@
 const chromium = require('@sparticuz/chrome-aws-lambda');
 const playwright = require('playwright-core');
+
+exports.handler = async(event, context, callback) => {
+    let browser = null;
+    let result = null;
+
+    try {
+        browser = await playwright.chromium.launch({
+            args: chromium.args,
+            executablePath: await chromium.executablePath,
+            headless: chromium.headless
+        });
+    } catch (e) {
+        return callback(e);
+    } finally {
+        if (browser !== null) {
+            await browser.close();
+        }
+    }
+};
